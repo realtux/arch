538a88bbb45a94d64cf66c9e4528979ca18f8378
Brian Seymour <imbrianseymour@gmail.com>
2014-09-18 16:22:21 -0500

    new run file, parse file, and updated test source

--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import sys
 import re
 
 import globals
+import parsetools
 
-from parsetools import *
-from modules.error import *
+import mod.error
+import mod.lexer
 
 globals.init()
 
@@ -28,37 +29,8 @@ for sourceline in arch_lines:
 
     source += str(sourceline.strip()) + '\n'
 
-i = 0
-
-# main lexer, parse every letter
-while i < len(source):
-    # eat all space encountered that isn't part of a string
-    if source[i] == ' ':
-        i += eat_space(source[i:])
-        continue
-
-    # end of line, push past and increment line
-    elif source[i] == '\n':
-        i += 1
-        globals.line += 1
-        continue
-
-    # comment, eat text up to newline
-    elif source[i:].startswith('//'):
-        #print globals.line, source[i:3]
-        i += eat_comment(source[i:])
-        continue
-
-    # assignments
-    elif re.search(r"^var [a-zA-Z0-9_]+\s*\=\s*.+\n", source[i:]):
-        i += handle_assignment(source[i:])
-
-    # constructs (out)
-    elif re.search(r"^out", source[i:]):
-        i += handle_construct(source[i:])
-
-    else:
-        err_fatal('Unrecognized symbol')
+# kick it off
+mod.lexer.lexer(source)
 
 # end execution
 print ''
