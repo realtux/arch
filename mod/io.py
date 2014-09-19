ee98d07d02ac1676c25e7f9d084c90d77350358e
Brian Seymour <imbrianseymour@gmail.com>
2014-09-19 01:21:01 -0500

    updated parsetools and lexer namespaces

--- mod/io.py
+++ mod/io.py
@@ -2,18 +2,18 @@ import re
 import string
 import sys
 
-from parsetools import *
+import parsetools
 
-from mod.error import *
-from mod.lexer import *
+import mod.error
+import mod.lexer
 
 def out(source):
     funcmeta = 0
 
     funcmeta += len(out.__name__)
-    funcmeta += eat_space(source[funcmeta:])
+    funcmeta += parsetools.eat_space(source[funcmeta:])
 
-    ccount, output = evaluate_expression(source[funcmeta:])
+    ccount, output = mod.lexer.evaluate_expression(source[funcmeta:])
 
     # convert the text representations of the newline to a real newline
     output = string.replace(output, '\\n', '\n')
