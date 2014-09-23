f291072f1a9404a3e904189499af0bbc48ed3ea2
Brian Seymour <imbrianseymour@gmail.com>
2014-09-22 23:46:26 -0500

    updated constructs

--- mod/constructs.py
+++ mod/constructs.py
@@ -2,6 +2,8 @@ import re
 
 import globals
 
+import parsetools
+
 import mod.error
 import mod.lexer
 
@@ -12,16 +14,14 @@ def is_construct(source):
 
     return False
 
-def handle_construct(source):
-    for construct in globals.constructs:
-        if source.startswith(construct):
-            callable = construct
-            break
+def handle_construct(source, construct):
+    if construct in globals.constructs:
+        callable = construct
 
     return globals.constructs[callable](source)
 
 def handle_assignment(source):
-    assignment = re.compile('^var ([a-zA-Z0-9_]+)\s*\=\s*(.+)\n').search(source)
+    assignment = re.compile('^var ([a-zA-Z][a-zA-Z0-9_]+?)\s*\=\s*(.+)\n').search(source)
 
     # assignment
     if assignment:
@@ -30,7 +30,12 @@ def handle_assignment(source):
 
         ccount, result = mod.lexer.evaluate_expression(expression)
 
-        globals.symbol_table['variables'][variable] = result
+        if type(result) is int:
+            actual_result = int(result)
+        else:
+            actual_result = str(result)
+
+        globals.symbol_table['variables'][variable] = actual_result
 
         i = 0
         while source[i] != '\n': i += 1
@@ -48,6 +53,8 @@ def handle_if(source):
 
     chars_eaten = 0;
 
+    chars_eaten += parsetools.eat_space(source)
+
     if_expression = re.compile(r"\((.+[^=!<>])([=!<>]+)(.+)\)").search(source);
 
     if if_expression:
@@ -57,9 +64,7 @@ def handle_if(source):
             err_fatal('Invalid conditional operator')
 
         ccount, expression1 = mod.lexer.evaluate_expression(if_expression.groups()[0])
-        chars_eaten += ccount
         ccount, expression2 = mod.lexer.evaluate_expression(if_expression.groups()[2])
-        chars_eaten += ccount
 
     operation = False
 
@@ -79,31 +84,52 @@ def handle_if(source):
             chars_eaten += 2
             operation = True
 
-    # eat space up to opening {
-    while (source[chars_eaten] != '{'):
-        chars_eaten += 1
+    chars_eaten += parsetools.eat_parens_block(source[chars_eaten:])
 
-    chars_eaten += 1
+    chars_eaten += parsetools.eat_space(source[chars_eaten:])
 
+    # if statement must contain at least 1 block either way
+    if source[chars_eaten] != '{':
+        mod.error.err_parse('{ Expected')
+
+    # process a true condition
     if operation == True:
+        chars_eaten += 1
         chars_eaten += mod.lexer.lexer(source[chars_eaten:])
-    else:
-        nested_ifs = 0
 
-        while source[chars_eaten] != '}' or nested_ifs != 0:
+        # eat the remaining else block if present
+        while True and chars_eaten < len(source):
+            chars_eaten += parsetools.eat_space(source[chars_eaten:])
+
             if source[chars_eaten] == '\n':
                 chars_eaten += 1
                 globals.line += 1
 
-            elif source[chars_eaten] == '{':
-                chars_eaten += 1
-                nested_ifs += 1
+            elif source[chars_eaten:].startswith('else'):
+                chars_eaten += 4
+                chars_eaten += parsetools.eat_space(source[chars_eaten:])
 
-            elif source[chars_eaten] == '}':
-                chars_eaten += 1
-                nested_ifs -= 1
+                if source[chars_eaten] == '{':
+                    chars_eaten += parsetools.eat_braced_block(source[chars_eaten:])
+                else:
+                    mod.error.err_parse('{ Expected')
 
             else:
-                chars_eaten += 1
+                break
 
-    return chars_eaten + 1
+    else:
+        chars_eaten += parsetools.eat_braced_block(source[chars_eaten:])
+
+        chars_eaten += parsetools.eat_space(source[chars_eaten:])
+
+        if source[chars_eaten:].startswith("else"):
+            chars_eaten += 4
+            chars_eaten += parsetools.eat_space(source[chars_eaten:])
+
+            if source[chars_eaten] == '{':
+                chars_eaten += 1
+                chars_eaten += mod.lexer.lexer(source[chars_eaten:])
+            else:
+                mod.error.err_parse('{ Expected')
+
+    return chars_eaten
