9d1d786a80aab5fba09f18094da18f7a4ceb8055
Brian Seymour <imbrianseymour@gmail.com>
2014-09-24 21:19:11 -0500

    parser update

--- parsetools.py
+++ parsetools.py
@@ -1,9 +1,13 @@
-# chew up the space until the next character that isn't a space
-# returns: number of spaces until the next character is hit
+import globals
+
+import mod.error
+
 def eat_space(source):
     i = 0
-
-    while source[i] == ' ': i += 1
+    try:
+        while source[i] == ' ': i += 1
+    except IndexError:
+        return 0
 
     return i
 
@@ -15,3 +19,51 @@ def eat_comment(source):
 
     # return number of characters and push past the newline
     return i
+
+def eat_braced_block(source):
+    i = 1
+
+    nested_braces = 0
+
+    try:
+        while source[i] != '}' or nested_braces != 0:
+            if source[i] == '\n':
+                i += 1
+                globals.line += 1
+
+            elif source[i] == '{':
+                i += 1
+                nested_braces += 1
+
+            elif source[i] == '}':
+                i += 1
+                nested_braces -= 1
+
+            else:
+                i += 1
+    except IndexError:
+        mod.error.err_parse('} Expected')
+
+    return i + 1
+
+def eat_parens_block(source):
+    i = 1
+
+    nested_parens = 0
+
+    try:
+        while source[i] != ')' or nested_parens != 0:
+            if source[i] == '(':
+                i += 1
+                nested_parens += 1
+
+            elif source[i] == ')':
+                i += 1
+                nested_parens -= 1
+
+            else:
+                i += 1
+    except IndexError:
+        mod.error.err_parse(') Expected')
+
+    return i + 1
