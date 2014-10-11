b165dd77130a8dd52077f8aa58dae1b09ece12cf
Brian Seymour <imbrianseymour@gmail.com>
2014-10-11 11:17:07 -0500

    clean up

--- mod/error.py
+++ mod/error.py
@@ -1,11 +1,11 @@
 import globals
 
+def err_warning(message):
+    print '\n','Arch Warning:', message, 'at line', str(globals.line)
+
 def err_fatal(message):
     print '\n','Arch Fatal:', message, 'at line', str(globals.line)
     quit()
-
-def err_warning(message):
-    print '\n','Arch Warning:', message, 'at line', str(globals.line)
 
 def err_parse(message):
     print '\n','Arch Parse Error:', message, 'at line', str(globals.line)
